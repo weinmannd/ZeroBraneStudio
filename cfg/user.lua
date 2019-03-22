6d6fb9d50d2f6bb4c0f2a83c429e719d260cf225
Franco Sinisi <franco@hyperganic.com>
2019-03-22 15:55:20 -0300

    SPROUT-882 Lua docs are being read from a json file.

--- cfg/user.lua
+++ cfg/user.lua
@@ -13,10 +13,10 @@ styles.auxwindow = styles.text -- apply text colors to auxiliary windows
 styles.calltip = styles.text -- apply text colors to tooltips
 
 styles.indicator.fncall = nil
-styles.indicator.varlocal = nil
-styles.indicator.varmasking = {fg = {255,0,0}};
-styles.indicator.varmasked = {fg = {255,0,0}};
-styles.indicator.varglobal =  { st = wxstc.wxSTC_INDIC_DOTS, fg = {82,80,80} };
+styles.indicator.varlocal = {fg = {80,80,80}}
+styles.indicator.varmasking = {fg = {255,0,0}}
+styles.indicator.varmasked = {fg = {255,0,0}}
+styles.indicator.varglobal =  nil -- do not underline global variables
 styles.fold = {fg = {42,43,44}, bg = {32,33,34} } -- fold color
 styles.indent.fg = {48, 49, 50} -- indent color
 styles.linenumber = { fg = {98,99,100}, bg = {32,33,34} }
@@ -31,3 +31,5 @@ acandtip.shorttip = false -- show short calltip when typing; set to false to sho
 output.fontname = "Open Sans"
 console.fontname = "Open Sans"
 filetree.fontname = "Open Sans"
+
+debugger.runonstart = true -- Don't stop on the first line when debugging starts; stop at first breakpoint
